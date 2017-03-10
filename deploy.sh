e1739a179ee20dcdc4ff79515c7f8dc3912d59d7
Miroslav Čillík <miro@keboola.com>
2017-03-10 15:39:51 +0100

    :bug: fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/google-drive-sheets quay.io/keboola/google-sheets-writer:$TRAVIS_TAG
+docker tag keboola/google-sheets-writer quay.io/keboola/google-sheets-writer:$TRAVIS_TAG
 docker images
 docker push quay.io/keboola/google-sheets-writer:$TRAVIS_TAG
