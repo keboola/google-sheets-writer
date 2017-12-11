939d3233d0c4500abe6ee378f3992c9aef628238
Miroslav Čillík <miro@keboola.com>
2017-12-11 09:53:56 +0100

    :cop: fixed codecoverage script

--- code-coverage.sh
+++ code-coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 composer install \
-&& ./vendor/bin/phpunit --coverage-clover build/logs/clover.xml
+&& ./vendor/bin/phpunit --coverage-clover build/logs/clover.xml \
 && ./vendor/bin/test-reporter
