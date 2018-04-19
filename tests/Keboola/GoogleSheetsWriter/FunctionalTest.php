1ab3c88a65ec711a4cf18a24ebfd2b26fc3df780
Miroslav Čillík <miro@keboola.com>
2018-04-19 14:55:33 +0200

    tests: test update wide sheet with shorter file

--- tests/Keboola/GoogleSheetsWriter/FunctionalTest.php
+++ tests/Keboola/GoogleSheetsWriter/FunctionalTest.php
@@ -254,7 +254,7 @@ class FunctionalTest extends BaseTest
                 '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
                 '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
             ]);
-        for ($i = 0; $i < 10000; $i++) {
+        for ($i = 0; $i < 5000; $i++) {
             $inputCsv->writeRow([
                 $i,
                 uniqid(), uniqid(),
